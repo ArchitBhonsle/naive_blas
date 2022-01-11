05a48742e815bbde28199d01ef292307d39d7206
Archit Bhonsle <abhonsle2000@gmail.com>
2022-01-11 11:00:16 +0530

    axpy

---
+++ src/level_1/axpy.rs
@@ -0,0 +1,115 @@
+use nd_slice::NdSliceMut;
+use num_complex::Complex;
+use num_traits::Float;
+
+/// Computes a vector-scalar product and adds the result to the vector.
+///
+/// y = ax + y
+///
+/// Input:
+///     n: number of elements in x and y
+///     a: scalar to multiply with
+///     x: vector
+///     incx: increment for x
+///     y: vector
+///     incy: increment for y
+/// Output:
+///     y: modified y
+pub fn axpy_real<T: Float>(
+    n: &isize,
+    a: &T,
+    x: &mut NdSliceMut<'_, T, 1>,
+    incx: &isize,
+    y: &mut NdSliceMut<'_, T, 1>,
+    incy: &isize,
+) {
+    let (n, incx, incy) = (*n, *incx, *incy);
+    if n < 0 {
+        return;
+    }
+    let n = n as usize;
+
+    if a == num_traits::zero() {
+        return;
+    }
+
+    if incx == 1 && incy == 1 {
+        for i in 0..n {
+            y[[i]] += a * x[[i]];
+        }
+    } else {
+        let mut ix = if incx < 0 {
+            (1 - (n as isize)) * incx
+        } else {
+            0
+        };
+        let mut iy = if incy < 0 {
+            (1 - (n as isize)) * incy
+        } else {
+            0
+        };
+
+        for _ in 0..n {
+            y[[ix as usize]] += a * x[[ix as usize]];
+
+            ix += incx;
+            iy += incy;
+        }
+    }
+}
+
+/// Computes a vector-scalar product and adds the result to the vector.
+///
+/// y = ax + y
+///
+/// Input:
+///     n: number of elements in x and y
+///     a: scalar to multiply with
+///     x: vector
+///     incx: increment for x
+///     y: vector
+///     incy: increment for y
+/// Output:
+///     y: modified y
+pub fn axpy_complex<T: Float>(
+    n: &isize,
+    a: &Complex<Float>,
+    x: &mut NdSliceMut<'_, Complex<T>, 1>,
+    incx: &isize,
+    y: &mut NdSliceMut<'_, Complex<T>, 1>,
+    incy: &isize,
+) {
+    let (n, incx, incy) = (*n, *incx, *incy);
+    if n < 0 {
+        return;
+    }
+    let n = n as usize;
+
+    if a.re.abs() + a.im.abs() == num_traits::zero() {
+        return;
+    }
+
+    if incx == 1 && incy == 1 {
+        for i in 0..n {
+            y[[i]] += a * x[[i]];
+        }
+    } else {
+        let mut ix = if incx < 0 {
+            (1 - (n as isize)) * incx
+        } else {
+            0
+        };
+        let mut iy = if incy < 0 {
+            (1 - (n as isize)) * incy
+        } else {
+            0
+        };
+
+        for _ in 0..n {
+            y[[ix as usize]] += a * x[[ix as usize]];
+
+            ix += incx;
+            iy += incy;
+        }
+    }
+}
---
+++ src/level_1/mod.rs
@@ -0,0 +1,8 @@
+pub mod axpy;
+pub mod copy;
+pub mod rot;
+pub mod rotg;
+pub mod rotm;
+pub mod rotmg;
+pub mod scal;
+pub mod swap;
